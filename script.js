9a5fafdecf07bf5423142db89692f45fdb5cbed8
Sujit Barman <42778388+sujitb368@users.noreply.github.com>
2023-10-05 11:50:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+// const box_1 = document.getElementById("box_1");
+// const box_2 = document.getElementById("box_2");
+// const box_5 = document.getElementById("box_5");
+// const box_6 = document.getElementById("box_6");
+// const box_3 = document.getElementById("box_3");
+// const box_4 = document.getElementById("box_4");
+// const box_7 = document.getElementById("box_7");
+// const box_8 = document.getElementById("box_8");
+// const box_9 = document.getElementById("box_9");
+// const box_10 = document.getElementById("box_10");
+// const box_11 = document.getElementById("box_11");
+// const box_12 = document.getElementById("box_12");
+
+
+
+// var nodelist = div.getElementsByTagName("img").length;
+// const div = ( ) =>{
+//     for(var i=1; i<=12; i++){
+//         let div = document.getElementById(`box_${i}`);
+//         console.log(div);
+//     }
+// }
+// div();
+// let ar = [];
+// let nam = "anim_1.jpg"
+// let type = ".jpg"
+// for (var i = 1; i <= 26; i++) {
+//     if (i < 10) {
+//         ar.push(`${nam}0${i}${type}`)
+//     } else {
+//         ar.push(`${nam}${i}${type}`)
+//     }
+// }
+
+// const len = ar.length;
+// let previous_img = null;
+// let display_img = (box) => {
+//     let rand = Math.floor(Math.random() * len);
+//     for (var i = 0; i < ar.length; i++) {
+//         let imge = ar[rand]
+//         if (previous_img != rand){
+//             // box.innerHTML = `<div class="image_container"> <img  src="./image/${imge}"> </div>`;
+
+//         }
+
+//     }
+//     previous_img = rand;
+//     setTimeout(() => {
+//         display_img(box);
+//     }, 3000);
+
+// }
+
+// calling for indivisual image div
+// display_img(box_1);
+// display_img(box_2);
+// display_img(box_3);
+// display_img(box_4);
+// display_img(box_5);
+// display_img(box_6);
+// display_img(box_7);
+// display_img(box_8);
+// display_img(box_9);
+// display_img(box_10);
+// display_img(box_11);
+// display_img(box_12);
+
